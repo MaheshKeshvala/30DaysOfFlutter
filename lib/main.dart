606d59559080d130a5ac950b1e13ad624f4d6880
Mahesh Keshvala <maheshkeshvala@sadmins-Mac-mini.local>
2021-05-20 15:44:46 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
+import 'package:demo_ch_1/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("My First Flutter App"),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
