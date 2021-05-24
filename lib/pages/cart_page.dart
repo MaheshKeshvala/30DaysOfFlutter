f0f5ad3e3c52df0895f55254b673a7e4e52baeb3
Mahesh Keshvala <maheshkeshvala>
2021-05-24 19:44:36 +0530

    place holder,cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -10,6 +10,56 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       backgroundColor: context.canvasColor,
+      body: Column(
+        children: [
+          CartListState().p32().expand(),
+          Divider(),
+          CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "Total : \$${999}".text.color(context.theme.accentColor).xl3.make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(context.theme.buttonColor),
+            ),
+            child: "Buy".text.color(context.theme.buttonColor).make(),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class CartListState extends StatelessWidget {
+  const CartListState({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Text List1".text.make(),
+      ),
     );
   }
 }
